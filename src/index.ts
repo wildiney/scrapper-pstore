e8229b520c0245ab6d617df3a2f38b815e28d306
Wildiney Di Masi <wildiney@gmail.com>
2022-06-03 16:11:21 -0300

    adapted to the new html structure from website

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import puppeteer from 'puppeteer'
 import autoScroll from './utils/puppeteer-autoscroll'
 import fs from 'fs'
 
-const search = encodeURI('agro');
+const search = encodeURI('agronegocio');
 
 (async () => {
   console.log('start')
-  const browser = await puppeteer.launch({ headless: true, devtools: false })
+  const browser = await puppeteer.launch({ headless: false, devtools: false })
   const page = await browser.newPage()
   await page.goto(`https://play.google.com/store/search?q=${search}&c=apps`, { waitUntil: 'networkidle2' })
   await autoScroll(page)
   const links = await page.evaluate(() => {
-    const alink: any = Array.from(document.querySelectorAll('.wXUyZd a'))
+    const alink: any = Array.from(document.querySelectorAll('.Si6A0c.Gy4nib'))
     return alink.map((a: any) => a.href)
   })
   const cleanedLinks: any = new Set(links)
@@ -28,22 +28,27 @@ const search = encodeURI('agro');
     let lastUpdate = null
     let installs: string | number | undefined = 0
 
-    try { title = await page.$eval('.AHFaub span', (item) => { return item.textContent }) } catch (e) { console.log(e) }
+    try { title = await page.$eval('h1', (item) => { return item.textContent }) } catch (e) { console.log(e) }
     try {
-      channel = await page.$eval('.qQKdcc span:first-child a', (item) => { return item.textContent })
-    } catch (e) { console.log(e) }
+      channel = await page.$eval('.Vbfug.auoIOc a', (item) => { return item.textContent })
+    } catch (e) { console.log('Error getting channel from ', title) }
     try {
-      genre = await page.$eval('.qQKdcc span:last-child a', (item) => { return item.textContent })
-    } catch (e) { console.log(e) }
+      genre = await page.$eval('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > c-wiz:nth-child(2) > div > section > div > div.Uc6QCc > div > div > span', (item) => { return item.textContent })
+    } catch (e) { console.log('Error getting genre from ', title) }
     try {
-      reviews = await page.$eval('#fcxH9b > div.WpDbMd > c-wiz > div > div.ZfcPIb > div > div > main > c-wiz:nth-child(1) > c-wiz:nth-child(1) > div > div.D0ZKYe > div > div.sIskre > div.jdjqLd > div.dNLKff > c-wiz > span > span:nth-child(1)', (item) => { return item.textContent?.replace('.', '') })
-    } catch (e) { console.log(e) }
+      reviews = await page.$eval('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div:nth-child(1) > div > div > c-wiz > div.hnnXjf > div.JU1wdd > div > div > div:nth-child(1) > div.g1rdde', (item) => { return item.textContent?.replace('.', '') })
+      if (reviews?.indexOf('avaliações') === -1) {
+        reviews = '0'
+      }
+      reviews = reviews?.replace(' mil avaliações', '')
+      reviews = parseInt(reviews!) * 1000
+    } catch (e) { console.log('Error getting reviews from ', title) }
     try {
-      stars = await page.$eval('.BHMmbe', (item) => { return item.textContent?.replace(',', '.') })
-    } catch (e) { console.log(e) }
+      stars = await page.$eval('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div:nth-child(1) > div > div > c-wiz > div.hnnXjf > div.JU1wdd > div > div > div:nth-child(1) > div.ClM7O > div > div', (item) => { return item.textContent?.replace(',', '.').replace('star', '') })
+    } catch (e) { console.log('Error getting stars from ', title) }
     // lastUpdate
     try {
-      lastUpdate = await page.$eval('#fcxH9b > div.WpDbMd > c-wiz > div > div.ZfcPIb > div > div > main > c-wiz:nth-child(3) > div.W4P4ne > div.JHTxhe.IQ1z0d > div > div:nth-child(1) > span > div > span', (item) => { return item.textContent })
+      lastUpdate = await page.$eval('.xg1aie', (item) => { return item.textContent })
     } catch (e) { console.log(e) }
     if (lastUpdate === null) {
       try {
@@ -52,13 +57,19 @@ const search = encodeURI('agro');
     }
     // installs
     try {
-      installs = await page.$eval('#fcxH9b > div.WpDbMd > c-wiz > div > div.ZfcPIb > div > div > main > c-wiz:nth-child(4) > div.W4P4ne > div.JHTxhe.IQ1z0d > div > div:nth-child(3) > span > div > span', (item) => { return item.textContent?.replace('+', '') })
-    } catch (e) { console.log(e) }
-    if (installs === 0) {
-      try {
-        installs = await page.$eval('#fcxH9b > div.WpDbMd > c-wiz > div > div.ZfcPIb > div > div > main > c-wiz:nth-child(3) > div.W4P4ne > div.JHTxhe.IQ1z0d > div > div:nth-child(3) > span > div > span', (item) => { return item.textContent?.replace('+', '') })
-      } catch (e) { console.log(e) }
-    }
+      installs = await page.$eval('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div:nth-child(1) > div > div > c-wiz > div.hnnXjf > div.JU1wdd > div > div > div:nth-child(1) > div.ClM7O', (item) => { return item.textContent?.replace('+', '') })
+      if (installs?.indexOf('star') === -1) {
+        installs = await page.$eval('#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.P9KVBf > div > div > c-wiz > div.hnnXjf.XcNflb.J1Igtd > div.JU1wdd > div > div > div:nth-child(2) > div.ClM7O', (item) => { return item.textContent?.replace('+', '') })
+      }
+      installs = installs!.replace(' mil', '')
+      installs = installs!.replace('mil', '')
+      installs = parseInt(installs) * 1000
+    } catch (e) { console.log('Error getting installs from ', title) }
+    // if (installs === 0) {
+    // try {
+    //   installs = await page.$eval('#fcxH9b > div.WpDbMd > c-wiz > div > div.ZfcPIb > div > div > main > c-wiz:nth-child(3) > div.W4P4ne > div.JHTxhe.IQ1z0d > div > div:nth-child(3) > span > div > span', (item) => { return item.textContent?.replace('+', '') })
+    // } catch (e) { console.log(e) }
+    // }
     const content = `${title};${channel};${genre};${reviews};${stars};${lastUpdate};${installs};${link}\n`
     fs.appendFileSync(`${decodeURI(search)}.csv`, content)
     apps.push({ title, channel, genre, reviews, stars, lastUpdate, installs, link })
